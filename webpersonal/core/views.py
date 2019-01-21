4b91100e982be867de4a3cc17c17e9033f55e83d
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-01-21 16:23:27 -0300

    Practica finalizada, Sección 2.11 'Paginas porfolio y contacto'

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -5,10 +5,31 @@ html_base = """
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Portafolio</a></li>
+    <li><a href="/contact/">Contácto</a></li>
+</ul>
 """
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base+"<h2>Portada</h2><p>Ejemplo parrafo portada</p>")
+    return HttpResponse(html_base+"""
+    <h2>Portada</h2>
+    <p>Ejemplo parrafo portada</p>
+    """)
 
 def about(request):
-    return HttpResponse("<h1>Web Personal </h1><h2>Acerca de</h2>")
+    return HttpResponse(html_base+"""
+    <h2>Acerca de</h2>
+    <p>Ejemplo parrafo about-me</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base+"""
+    <h2>Portafolio</h2>
+    <p>Ejemplo parrafo portafolio</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base+"""
+    <h2>Contacto</h2>
+    <p>Ejemplo parrafo contacto</p>
+    """)
