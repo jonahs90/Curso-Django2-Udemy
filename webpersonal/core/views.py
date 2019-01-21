15aaa0be0e68786eae0aafdab8a5da4e25f6be52
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-01-21 16:05:58 -0300

    Pruebas con html_base devuelto desde la view usando HttpResponse

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>Web Personal </h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+"""
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Web Personal </h1><h2>Portada</h2>")
+    return HttpResponse(html_base+"<h2>Portada</h2><p>Ejemplo parrafo portada</p>")
+
+def about(request):
+    return HttpResponse("<h1>Web Personal </h1><h2>Acerca de</h2>")
