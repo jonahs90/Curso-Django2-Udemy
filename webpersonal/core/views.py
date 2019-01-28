a2c54cca024c173d54934a3c8b5ce045cb62bdb2
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-01-28 12:30:28 -0300

    Uso de templates en vez de HTMLResponses

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,35 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Web Personal </h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contácto</a></li>
-</ul>
-"""
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base+"""
-    <h2>Portada</h2>
-    <p>Ejemplo parrafo portada</p>
-    """)
+    return render(request, 'core/home.html')
 
 def about(request):
-    return HttpResponse(html_base+"""
-    <h2>Acerca de</h2>
-    <p>Ejemplo parrafo about-me</p>
-    """)
+    return render(request, 'core/about.html')
 
 def portfolio(request):
-    return HttpResponse(html_base+"""
-    <h2>Portafolio</h2>
-    <p>Ejemplo parrafo portafolio</p>
-    """)
+    return render(request, 'core/portfolio.html')
 
 def contact(request):
-    return HttpResponse(html_base+"""
-    <h2>Contacto</h2>
-    <p>Ejemplo parrafo contacto</p>
-    """)
+    return render(request, 'core/contact.html')
