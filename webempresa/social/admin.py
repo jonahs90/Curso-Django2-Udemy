61bba237fdf3beb820bc7a0fc78be1ef63c68b36
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-02-21 15:22:06 -0300

    Creando sistema de usuarios en admin

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -5,4 +5,16 @@ from .models import Link
 class AdminLink(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Personal").exists():
+             # Comprobara si dentro de el grupo personal existe el usuario
+            return('name',)
+        else:
+            return ('created', 'updated')
+
+    def get_exclude(self, request, obj=None):
+         if request.user.groups.filter(name="Personal").exists():
+             # Comprobara si dentro de el grupo personal existe el usuario
+            return('key',)
+
 admin.site.register(Link, AdminLink)
