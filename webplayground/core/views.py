993a3c59451f17b6b991edfd26026392625949e1
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-03-01 15:11:54 -0300

    Modificando contexto en Home usando redefinicion del metodo get()

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -5,12 +5,8 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = "core/home.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = "WEB PLAYGROUND 2.0"
-        return context
-
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'title':"WEB PLAYGROUND 3.0"})
+
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
