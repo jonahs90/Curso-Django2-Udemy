4e52504f0c3779f9c2f9cc5aa6a724c2474d4d01
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-03-01 16:27:24 -0300

    Se añade vista Create View para model Page

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PagesListView, PagesDetailView
+from .views import PagesListView, PagesDetailView, PageCreate
 
-urlpatterns = [
+pages_patterns = ([
     path('', PagesListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PagesDetailView.as_view(), name='page'),
-]
+    path('create/', PageCreate.as_view(), name='create'),
+], 'pages')
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -2,7 +2,8 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse,reverse_lazy
 from .models import Page
 
 # Create your views here.
@@ -11,3 +12,8 @@ class PagesListView(ListView):
 
 class PagesDetailView(DetailView):
     model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title','content', 'order']
+    success_url = reverse_lazy('pages:pages')
