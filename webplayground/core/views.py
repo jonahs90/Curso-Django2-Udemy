14e982a4dde29feb51731d758bfabd9a00791633
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-03-01 14:48:48 -0300

    Prueba pasando context usando funcion get_context_data

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
 
-def sample(request):
-    return render(request, "core/sample.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "WEB PLAYGROUND 2.0"
+        return context
+
+    def get(self, request):
+        return render(request, self.template_name, {'title':"WEB PLAYGROUND 3.0"})
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
