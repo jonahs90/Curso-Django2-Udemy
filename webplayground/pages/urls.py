19f1c6a44a1b97e7e8e86978243b382dd21f6015
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-03-07 11:28:55 -0300

    Vistas CRUD: Updatae view añadido

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PagesListView, PagesDetailView, PageCreate
+from .views import PagesListView, PagesDetailView, PageCreate, PageUpdate
 
 pages_patterns = ([
     path('', PagesListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PagesDetailView.as_view(), name='page'),
     path('create/', PageCreate.as_view(), name='create'),
+    path('update/<int:pk>', PageUpdate.as_view(), name='update'),
 ], 'pages')
