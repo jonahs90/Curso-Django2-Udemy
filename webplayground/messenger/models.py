21fcbe244ad9bba9902297d4507da6c8b38ba67b
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-03-22 16:47:12 -0300

    Se agrega primer test que falla, se refactoriza models mediante señales para corregir el fallo

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
+
 # Create your models here.
 
 class Message(models.Model):
@@ -13,3 +15,22 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name="threads")
     messages = models.ManyToManyField(Message)
+
+
+def messages_changed(sender, *args, **kwargs):
+    instance = kwargs.pop("instance", None)
+    action = kwargs.pop("action", None)
+    pk_set = kwargs.pop("pk_set", None)
+    print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action is "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print("Usuario {} no forma parte del hilo".format(msg.user))
+                false_pk_set.add(msg_pk)
+    #Buscar los mensaje de false_pk_set y los borramos de pk_set
+    pk_set.difference_update(false_pk_set)
+    
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
