de35a1d3193993e06bd70a26aa00182b6c888005
Jonathan Sanchez <jonathansanchez595@gmail.com>
2019-03-26 12:05:21 -0300

    Se crea la carga de los mensajes de manera asincrona

--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
-from .models import Thread
+from .models import Thread, Message
 from django.http import Http404, JsonResponse
 
 from django.contrib.auth.decorators import login_required
@@ -24,6 +24,15 @@ class ThreadDetail(DetailView):
         return obj
 
 def add_message(request, pk):
-    print(request.GET)
     json_response = {'created': False}
+    if request.user.is_authenticated:
+        content = request.GET.get('content', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)
+            message = Message.objects.create(user=request.user, content=content)
+            thread.messages.add(message)
+            json_response = {'created': True}
+    else:
+        raise Http404("User is not authenticated")
+
     return JsonResponse(json_response)
